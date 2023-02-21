23d246626bac69d30be4d991a4aab0b96d38a70b
SergeiShilkoysky <sergshrb79@gmail.com>
2023-02-21 22:13:56 +0300

    №30_Вывод массива из 8 элю-в заполн. 0 и 1 в случ. порядке

---
+++ L_2/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3  /  78 -> 2 / 89126 -> 5
+
+// 1 способ
+int GetRange (int value)
+{
+    int i = 0;
+    if (value != 0)
+    {
+        while(value > 0)
+        {
+            i++;
+            value = value / 10;
+        }
+        return i;
+    }
+    else return 1;
+}
+int a;
+Console.Write("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetRange(a));
+
+
+// 2 способ
+string number;
+Console.Write("Введите число: ");
+number = Console.ReadLine();
+Console.WriteLine(number.Length);
+
+
+// 3 способ
+int GetLenghtNumberToStr(int value) 
+{
+    string spamNumber = value.ToString();
+    return spamNumber.Length;
+}
+int GetLenghtNumberMath(int value)
+{
+    if( value == 0)
+    {
+        return 1;
+    }
+    
+    int count = 0;
+    for(int i = 0; value >= 1; i++)
+    {
+        value /= 10;
+        count++;
+    }
+    return count;
+}
+int[] arrNums = new int[] {456, 78, 89126, 100, 0, 11};
+Console.WriteLine($"Метод строк");
+foreach (int num in arrNums)
+{
+    Console.WriteLine($"Длина числа {num}, равна {GetLenghtNumberToStr(num)}.");
+}
+Console.WriteLine($"");
+Console.WriteLine($"Метод математики");
+foreach (int num in arrNums)
+{
+    Console.WriteLine($"Длина числа {num}, равна {GetLenghtNumberMath(num)}.");
+}
+
---
+++ L_3/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число M и N и выдаёт произведение чисел от M до N.
+//2,4 -> 24 / 1,5 -> 120
+
+// 1 способ
+int MultNum(int a, int b)
+{
+    int result;
+    int i;
+    result = 1;
+    for (i = a; i <= b; i++)
+    {
+        result = i * result;
+    }
+
+    return result;
+}
+
+int n;
+int m;
+Console.Clear();
+Console.Write("введите число n ");
+n = int.Parse(Console.ReadLine());
+Console.Write("введите число m ");
+m = int.Parse(Console.ReadLine());
+Console.WriteLine($"произведение чисел от {Math.Min(n, m)} до {Math.Max(n, m)} = {MultNum(Math.Min(n, m), Math.Max(n, m))}");
+
+
+// 2 способ
+int GetMultNum(int value, int mult)
+{
+    // если что то 0 то все ноль
+    if (mult == 0 || value == 0)
+    {
+        return 0;
+    }
+
+    // Как обработать ошибку диапазона????!
+    if (mult < value)
+    {
+        Console.Write("Ошибка диапазона! ");
+        return -1;
+    }
+
+    // вычисляет произведение в диапазоне от М до N
+    int spamValue = 1;
+
+    for (int i = value; i <= mult; i++)
+    {
+        spamValue *= i;
+    }
+
+    return spamValue;
+}
+
+Console.WriteLine($"Произведение чисел равно {GetMultNum(2, 4)}");
+Console.WriteLine($"Произведение чисел равно {GetMultNum(1, 5)}");
+
+// эксперементы
+Console.WriteLine($"");
+Console.WriteLine($"Произведение чисел равно {GetMultNum(-1, 5)}");
+Console.WriteLine($"Произведение чисел равно {GetMultNum(-5, -1)}");
+Console.WriteLine($"Произведение чисел равно {GetMultNum(-1, -5)}");
+
+
