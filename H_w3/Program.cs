cbb532f3f61c2a418f4e8c53306f085d24748c62
SergeiShilkoysky <sergshrb79@gmail.com>
2023-02-25 16:51:59 +0300

    Сумма чисел от 1 до A

---
+++ H_w3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//  1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]  /    6, 1, 33 -> [6, 1, 33]
+
+int[] FillArray(int array)
+{
+    array = int [array.Length];
+        Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = rnd.Next(0, 100);
+    }
+    return result;
+}
+void PrintArray(){
+     Console.WriteLine($"[{string.Join(",",array)}]");
+}
+ Console.WriteLine($"Введите размер массива:");
+int size = int.Parse(Console.ReadLine());
+
+int[] array = FillArray(array);
+print(array);
+
+
+
+
--- L_1/Program.cs
+++ L_1/Program.cs
@@ -1,14 +1,14 @@
 ﻿//Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
 //7 -> 28  / 4 -> 10  / 8 -> 36
-
+/*
 Console.Clear();
 
 Console.WriteLine("Введите число А:");
 
 int a = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"Сумма чисел от 1 до {a} = {GetSumByNumber(a)}");
-
+Console.WriteLine($"Сумма чисел от 1 до {a} = {GetSumByNumber(a)}"); // вместо 10 строки можно использ. 11 и 21 22
+// PrintResul();
 int GetSumByNumber(int limit)
 {
     int result = 0;
@@ -17,4 +17,22 @@ int GetSumByNumber(int limit)
         result += i; //=  result = result + i;
     }
     return result;
+}
+//void PrintResult(){
+ //   Console.WriteLine($"Сумма чисел от 1 до {a} = {GetSumByNumber(a)}");
+//}
+*/
+
+int a = new Random().Next(0, 10);
+Console.WriteLine($"Число А: {a}");
+a = 3;
+Console.WriteLine($"Сумма чисел от 1 до {a} = {GetSum(a)}");
+int GetSum (int num)
+{
+    int sum = 0;
+for (int i = 1; i <= num; i++)
+{
+    sum += i;
+}
+return sum;
 }
